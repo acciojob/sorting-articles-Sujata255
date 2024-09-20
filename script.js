5f4851bacc1d8ea420b955b46fe2e5d523335cf6
Sujata255 <das19sujata@gmail.com>
2024-09-20 19:35:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway   
+ State",
+  "we are at Kona",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog"
+];
+
+const stopWords = ["a", "an", "the"];
+
+const sortedBands = bands
+  .filter(band => !stopWords.includes(band.toLowerCase()))
+  .sort();
+
+const bandList = document.getElementById("band");
+
+sortedBands.forEach(band => {
+  const listItem = document.createElement("li");
+  listItem.textContent = band;
+  bandList.appendChild(listItem);   
+
+});
